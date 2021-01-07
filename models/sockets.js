d99a6b87f5d227883ee3a072e9c208f87180806f
adriton <adrtler@gmail.com>
2021-01-07 18:17:10 +0100

    Modelos backend

--- models/sockets.js
+++ models/sockets.js
@@ -1,31 +1,21 @@
-
+const Marcadores = require('./marcadores');
 
 class Sockets {
+  constructor(io) {
+    this.io = io;
 
-    constructor( io ) {
+    this.marcadores = new Marcadores();
+    this.socketEvents();
+  }
 
-        this.io = io;
-
-        this.socketEvents();
-    }
-
-    socketEvents() {
-        // On connection
-        this.io.on('connection', ( socket ) => {
-
-            // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( data ) => {
-                console.log( data );
-                
-                this.io.emit('mensaje-from-server', data );
-            });
-            
-        
-        });
-    }
-
-
+  socketEvents() {
+    // On connection
+    this.io.on('connection', (socket) => {
+      // TODO: marcadores-activos
+      // TODO: marcador-nuevo
+      // TODO: marcador-actualizado
+    });
+  }
 }
-
 
 module.exports = Sockets;
